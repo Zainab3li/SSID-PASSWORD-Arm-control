7d26ae01af10e805d25ffdeaa0257873b293dfab
Zainab3li <85633968+Zainab3li@users.noreply.github.com>
2021-08-10 14:34:15 +0300

    Add files via upload

---
+++ C.js
@@ -0,0 +1,59 @@
+
+var slider=document.getElementById("R1");
+var output=document.getElementById("Output1");
+
+output.innerHTML=slider.value;
+slider.oninput= function(){
+    output.innerHTML=this.value;
+
+}
+
+var slider2=document.getElementById("R2");
+var output2=document.getElementById("Output2");
+
+output2.innerHTML=slider2.value;
+slider2.oninput= function(){
+    output2.innerHTML=this.value;
+
+}
+
+
+var slider3=document.getElementById("R3");
+var output3=document.getElementById("Output3");
+
+output3.innerHTML=slider3.value;
+slider3.oninput= function(){
+    output3.innerHTML=this.value;
+
+}
+
+
+var slider4=document.getElementById("R4");
+var output4=document.getElementById("Output4");
+
+output4.innerHTML=slider4.value;
+slider4.oninput= function(){
+    output4.innerHTML=this.value;
+
+}
+
+
+var slider5=document.getElementById("R5");
+var output5=document.getElementById("Output5");
+
+output5.innerHTML=slider5.value;
+slider5.oninput= function(){
+    output5.innerHTML=this.value;
+
+}
+
+
+
+var slider6=document.getElementById("R6");
+var output6=document.getElementById("Output6");
+
+output6.innerHTML=slider6.value;
+slider6.oninput= function(){
+    output6.innerHTML=this.value;
+
+}
